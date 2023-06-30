4b0957e9e327cc2f916787959f6346943c5ff90a
akatheos <akatheos11@gmail.com>
2023-06-30 22:24:13 +0100

    fixed a phrase that wasn't right in signin component

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -8,7 +8,9 @@ const SignIn: FC = ({}) => {
     <div className="container mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
       <div className="flex flex-col space-y-2 text-center">
         <Icons.logo className="mx-auto h-6 w-6" />
-        <h1 className="text-2xl font-bold tracking-tight">Welcome back</h1>
+        <h1 className="text-2xl font-bold tracking-tight">
+          Welcome to Breadit
+        </h1>
         <p className="text-sm max-w-xs mx-auto">
           By continuing, you are setting up a breadit account and agree to our
           User Agreement and Privacy Policy.
